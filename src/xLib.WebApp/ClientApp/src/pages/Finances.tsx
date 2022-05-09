d1f10671a906e6f055c00ef888b44effc4e7bafb
w0rldx <michaelgmx1@googlemail.com>
2022-05-09 14:30:42 +0200

    ClientApp: Make changes for school project

--- src/xLib.WebApp/ClientApp/src/pages/Finances.tsx
+++ src/xLib.WebApp/ClientApp/src/pages/Finances.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { useStyles } from '../styles/FinancePageStyle';
 
 function Finances() {
-    return <div>Finances</div>;
+    const { classes } = useStyles();
+    return (
+        <>
+            <div className={classes.headerText}>Finanzdienstleister Reibach&Shöhne</div>
+            <hr />
+            <ul>
+                <li>Aktien</li>
+                <li>Immobilien</li>
+                <li>Sparkonto</li>
+            </ul>
+
+            <p>
+                Und jetzt kommen unsere wirklich prakischen tipps <br />
+                Sie wollen doch ihr Geld vermehren! <a href="aktien">Aktien</a>
+                <br />
+                Ihr Geld soll doch sicher sein! <a href="sparkonto">Sparkonto</a>
+                <br />
+                BetonGold! <a href="immobilien">Immobilien</a>
+            </p>
+        </>
+    );
 }
 
 export default Finances;
---
+++ src/xLib.WebApp/ClientApp/src/styles/FinancePageStyle.ts
@@ -0,0 +1,10 @@
+import { createStyles } from '@mantine/core';
+
+const useStyles = createStyles((theme) => ({
+    headerText: {
+        fontSize: '1.5rem',
+        fontWeight: 'bold',
+    },
+}));
+
+export { useStyles };
